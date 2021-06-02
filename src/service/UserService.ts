f1e277f0489b1ec0e3bccfe6ce32b29a168b7acf
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-02 14:32:39 -0300

    crud user service

---
+++ src/service/UserService.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
+
+import '../connection/connection'
+
+export class UserService {
+
+    async viewUser(request: Request, response: Response) {
+
+        const userRepository = getRepository(User)
+
+        const results = await userRepository.find()
+
+        response.status(200).json(results)
+    }
+
+    async saveUser(request: Request, response: Response) {
+
+        const userRepository = getRepository(User)
+
+        const user: User = request.body 
+
+        const validation = await userRepository.find({ where: { email: user.email}})
+
+        if (validation[0] == null) {
+
+            const results = await userRepository.save(user)
+
+            response.status(201).json(results)
+        
+        } else {
+
+            response.status(500).json({message: 'Este e-mail já está cadastrado.'})
+        }        
+    }
+
+    async editUser(request: Request, response: Response) {
+
+        const userRepository = getRepository(User)
+
+        const validation = await userRepository.find({ where: { email: request.body.email}})
+
+        if (validation[0] == null) {
+
+            const { name, email, password } = request.body
+            const id_user = request.params.id_user
+
+            await userRepository.update(id_user, { name: name, email: email, password: password })
+
+            response.status(204).end()
+        
+        } else {
+
+            response.status(500).json({message: 'Este e-mail já está cadastrado.'})
+        }    
+    }
+
+    async deleteUser(request: Request, response: Response) {
+
+        const userRepository = getRepository(User)
+
+        const id_user = request.params.id_user
+
+        const validation = await userRepository.find({ where: { id: id_user} })
+
+        if (validation[0] == null) {
+
+            response.status(500).json({message: 'Este usuário não existe.'})
+            
+        } else {
+
+            await userRepository.delete(id_user)
+
+            response.status(204).end()
+        }
+    }
+}
