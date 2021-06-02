05bed8b091ef630b916e626fdf42ce7adfd88ec2
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-02 15:57:47 -0300

    configuração de acesso a rotas

--- src/route/UserRoute.ts
+++ src/route/UserRoute.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import { auth } from '../middlewares/auth'
 import { UserService } from '../service/UserService'
 
 export const userRoute = express.Router()
 
 const userService = new UserService()
 
-userRoute.get('/users', userService.viewUser)
+userRoute.post('/session', userService.login)
 userRoute.post('/users', userService.saveUser)
+
+userRoute.use(auth)
+
+userRoute.get('/users', userService.viewUser)
 userRoute.put('/users/:id_user', userService.editUser)
 userRoute.delete('/users/:id_user', userService.deleteUser)
