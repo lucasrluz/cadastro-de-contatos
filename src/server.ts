10dfdab3b60a234f329b451915de7f92fcdb8e8d
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-05 13:18:21 -0300

    abilitar rota de contato

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { userRoute } from './route/UserRoute'
+import { contactRoute } from './route/contactRoute'
 
 const app = express()
 
 app.use(express.json())
 app.use(userRoute)
+app.use(contactRoute)
 
 app.listen(3333)
