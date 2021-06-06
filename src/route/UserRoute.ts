dd4366f49b7d3f5a4df89fefd148f778f7d0cd30
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-06 17:21:23 -0300

    remoção de rotas relacionadas ao jwt

--- src/route/UserRoute.ts
+++ src/route/UserRoute.ts
@@ -1,16 +1,11 @@
 import express from 'express'
-import { auth } from '../middlewares/auth'
 import { UserService } from '../service/UserService'
 
 export const userRoute = express.Router()
 
 const userService = new UserService()
 
-userRoute.post('/session', userService.login)
-userRoute.post('/users', userService.saveUser)
-
-userRoute.use(auth)
-
 userRoute.get('/users', userService.viewUser)
+userRoute.post('/users', userService.saveUser)
 userRoute.put('/users/:id_user', userService.editUser)
 userRoute.delete('/users/:id_user', userService.deleteUser)
