9a8d73b1e24a574297f47af80e7e1e80af6c8911
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-06 23:13:23 -0300

    refatoração na camada de service em contato

--- src/service/ContactService.ts
+++ src/service/ContactService.ts
@@ -2,104 +2,123 @@ import { Request, Response } from "express";
 import { getCustomRepository, getRepository, Not } from "typeorm";
 import { Contact } from "../entity/Contact";
 import { User } from "../entity/User";
+import { basicAuth } from "../middlewares/basicAuth";
 import { UserRepository } from "../repository/UserRepository";
-
 
 export class ContactService {
 
     async viewContact(request: Request, response: Response){
 
-        const userRepository = getCustomRepository(UserRepository)
-
         const contactRepository = getRepository(Contact)
 
-        const { email, password } = request.body
+        const validation = await basicAuth(request, response)
 
-        const validation = await userRepository.findByEmailAndPassword(email, password)
-        
-        if (validation == false) {
+        if (validation == 'É necessário o Header de Autenticação.') {
 
-            return response.status(404).json({ message: 'Este usuário não existe.' })
-            
-        } else {
-            
-            const contacts = await contactRepository.find({ where: { user: validation }})
-
-            return response.status(200).json(contacts)
+            return response.status(401).json({ message: validation })
         }
+
+        if (validation == 'Este usuário não existe.') {
+
+            return response.status(404).json({ message: validation })
+        }
+
+        const contacts = await contactRepository.find({ where: { user: validation }})
+        
+        return response.status(200).json(contacts)   
     }
 
     async saveContact(request: Request, response: Response){
         
         const contactRepository = getRepository(Contact)
-        const userRepository = getRepository(User)
 
-        const { number, id_user } = request.body
+        const userValidation = await basicAuth(request, response)
+
+        if (userValidation == 'É necessário o Header de Autenticação.') {
+
+            return response.status(401).json({ message: userValidation })
+        }
+
+        if (userValidation == 'Este usuário não existe.') {
+
+            return response.status(404).json({ message: userValidation })
+        }
 
         const contact: Contact = request.body
 
-        const user = await userRepository.find({ where: { id: id_user }})
+        const contactValidation = await contactRepository.findOne({ where: { number: contact.number, user: userValidation }})
 
-        const validation = await contactRepository.find({ where: { number: number }})
+        if (contactValidation == null) {
 
-        if (validation[0] == null) {
-
-            contact.user = user[0]
+            contact.user = userValidation
 
             const result = await contactRepository.save(contact)
 
             return response.status(201).json(result)
         
-        } else {
-
-            return response.status(500).json({message: 'Este contato já existe.'})
-        }
+        } 
+        
+        return response.status(500).json({message: 'Este contato já existe.'})
     }
 
     async editContact(request: Request, response: Response){
         
         const contactRepository = getRepository(Contact)
 
-        const { id_contact, firstName, lastName, number } = request.body
+        const userValidation = await basicAuth(request, response)
 
-        const validation = await contactRepository.find({ where: { number: number, id: Not(id_contact) }})
+        if (userValidation == 'É necessário o Header de Autenticação.') {
 
-        if (validation[0] == null) {
+            return response.status(401).json({ message: userValidation })
+        }
+
+        if (userValidation == 'Este usuário não existe.') {
+
+            return response.status(404).json({ message: userValidation })
+        }
+
+        const { firstName, lastName, number } = request.body
+        const id_contact = request.params.id_contact
+
+        const contactValidation = await contactRepository.findOne({ where: { number: number, user: userValidation, id: Not(id_contact) }})
+
+        if (contactValidation == null) {
 
             const result = contactRepository.update(id_contact, { firstName: firstName, lastName: lastName, number: number })
 
             return response.status(201).json(result)
         
-        } else {
+        } 
 
-            return response.status(500).json({ message: 'Este contato já existe.' })
-        }
+        return response.status(500).json({ message: 'Este contato já existe.' })
     }
 
     async deleteContact(request: Request, response: Response){
 
         const contactRepository = getRepository(Contact)
-        const userRepository = getCustomRepository(UserRepository)
 
-        const { id_contact, email, password } = request.body
+        const userValidation = await basicAuth(request, response)
         
-        const validationUser = await userRepository.findByEmailAndPassword(email, password)
+        if (userValidation == 'É necessário o Header de Autenticação.') {
 
-        if (validationUser == false) {
+            return response.status(401).json({ message: userValidation })
+        }
 
-            return response.status(404).json({ message: 'Este usuário não existe.' })
+        if (userValidation == 'Este usuário não existe.') {
+
+            return response.status(404).json({ message: userValidation })
+        }
+
+        const id_contact = request.params.id_contact
         
-        } 
-        
-        const validationContact = await contactRepository.find({ where: { id: id_contact, user: validationUser }})
+        const contactValidation = await contactRepository.findOne({ where: { id: id_contact, user: userValidation }})
 
-        if (validationContact[0] == null) {
+        if (contactValidation == null) {
 
             return response.status(404).json({ message: 'Este contato não existe.' })
-        
         } 
 
-        const result = contactRepository.delete({ id: id_contact, user: validationUser })
+        const result = await contactRepository.delete(id_contact)
 
         return response.status(204).json(result)
     }
