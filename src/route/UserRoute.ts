229046725ba7221f3860cefb13f1c2f9540ee277
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-07 14:12:40 -0300

    remoção do id na url

--- src/route/UserRoute.ts
+++ src/route/UserRoute.ts
@@ -7,5 +7,5 @@ const userService = new UserService()
 
 userRoute.get('/users', userService.viewUser)
 userRoute.post('/users', userService.saveUser)
-userRoute.put('/users/:id_user', userService.editUser)
-userRoute.delete('/users/:id_user', userService.deleteUser)
+userRoute.put('/users', userService.editUser)
+userRoute.delete('/users', userService.deleteUser)
