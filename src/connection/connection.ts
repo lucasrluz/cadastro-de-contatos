affc0504a0c64deb88320075e3bca5b7f6f6c1a1
lucasrluz <lucasr.luzbr@gmail.com>
2021-06-07 14:09:48 -0300

    mudança na mensagem de conexão com o banco de dados

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,3 +1,3 @@
 import { createConnection } from "typeorm";
 
-createConnection().then(() => {console.log('Conexão com Banco de Dados estabelecida.')})
+createConnection().then(() => {console.log('Conexão realizada com Banco de Dados.')})
